3b416a34670f2e494881569cef67ba20c6f66b11
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2025-05-03 18:38:13 -0700

    test(back-end): added skipped TextSnippet tests
    Added tests for getAllForExperience and getAllForExperienceInDocument
    in TextSnippet model.
    
    Also removed variables ClassRef and className, because the file is
    specific to TextSnippet.  This should make reading the test file easier.
    
    Also rewrote the beforeAll block for the TextSnippet describe block.
    The raw SQL statement is replaced with a User.register call to make the
    code shorter.  The then...catch chaining has been replaced with
    async/await to make the code more readable.

--- models/textSnippetSpecific.test.js
+++ models/textSnippetSpecific.test.js
@@ -1,10 +1,20 @@
 'use strict';
 
 const db = require('../database/db');
+const Experience = require('./experience');
+const Experience_X_Text_Snippet = require('./experience_x_textSnippet');
+const Document = require('./document');
+const Document_X_Experience = require('./document_x_experience');
 const TextSnippet = require('./textSnippet');
-
 const User = require('./user');
-const { users } = require('../_testData');
+
+const {
+  users,
+  documents,
+  experiences,
+  documents_x_experiences,
+  textSnippets,
+} = require('../_testData');
 const {
   dataForNewInstances,
   dataForUpdate,
@@ -16,35 +26,152 @@ const {
   clearTable,
 } = require('../_testCommon');
 
-// ================================================== Set Up Variables
-
-const ClassRef = TextSnippet;
-const className = 'TextSnippet';
-
 // ================================================== Specific Tests
 
-describe(className, () => {
+describe('TextSnippet', () => {
   // To help with expects by directly getting data from the database.
   const sqlTextSelectAll = `
-  SELECT ${ClassRef._allDbColsAsJs}
+  SELECT ${TextSnippet._allDbColsAsJs}
   FROM ${TextSnippet.tableName}`;
 
-  beforeAll(() =>
-    commonBeforeAll(db).then(() =>
-      db.query({
-        queryConfig: {
-          text: `
-  INSERT INTO ${User.tableName}
-  VALUES ($1, $2);`,
-          values: [users[0].username, users[0].password],
-        },
-      })
-    )
-  );
+  beforeAll(async () => {
+    await commonBeforeAll(db);
+
+    await User.register(users[0]);
+  });
 
   beforeEach(() => clearTable(db, TextSnippet.tableName));
 
   afterAll(() => commonAfterAll(db));
+
+  // -------------------------------------------------- getAllForExperience
+
+  describe('getAllForExperience', () => {
+    let addedExperience;
+    let addedDocumentXExperience;
+
+    beforeAll(async () => {
+      await Document.add(documents[0]);
+      addedExperience = await Experience.add(experiences[0]);
+      addedDocumentXExperience = await Document_X_Experience.add(
+        documents_x_experiences[0]
+      );
+    });
+
+    afterAll(async () => {
+      await clearTable(db, Document.tableName);
+      await clearTable(db, Experience.tableName);
+    });
+
+    test.each([
+      [0, []],
+      [textSnippets.length, textSnippets],
+    ])(
+      'Gets all %i text snippets for an experience.',
+      async (_length, textSnippetsToAdd) => {
+        // Arrange
+        let nextPosition = 0;
+
+        for (const textSnippetToAdd of textSnippetsToAdd) {
+          const addedTextSnippet = await TextSnippet.add(textSnippetToAdd);
+          await Experience_X_Text_Snippet.add({
+            documentXExperienceId: addedDocumentXExperience.id,
+            textSnippetId: addedTextSnippet.id,
+            textSnippetVersion: addedTextSnippet.version,
+            position: nextPosition++,
+          });
+        }
+
+        // Act
+        const textSnippets = await TextSnippet.getAllForExperience(
+          users[0].username,
+          addedExperience.id
+        );
+
+        // Assert
+        const expectedTextSnippets = textSnippetsToAdd.map(
+          (textSnippetToAdd) =>
+            new TextSnippet(
+              expect.any(Number),
+              expect.any(Date),
+              textSnippetToAdd.owner,
+              null,
+              textSnippetToAdd.type,
+              textSnippetToAdd.content
+            )
+        );
+
+        expect(textSnippets).toStrictEqual(expectedTextSnippets);
+      }
+    );
+  });
+
+  // -------------------------------------------------- getAllForExperience
+
+  describe('getAllForExperienceInDocument', () => {
+    let addedDocument;
+    let addedExperience;
+    let addedDocumentXExperience;
+
+    beforeAll(async () => {
+      addedDocument = await Document.add(documents[0]);
+      await Document.add(documents[1]);
+      addedExperience = await Experience.add(experiences[0]);
+      await Experience.add(experiences[1]);
+      addedDocumentXExperience = await Document_X_Experience.add(
+        documents_x_experiences[0]
+      );
+      await Document_X_Experience.add(documents_x_experiences[1]);
+    });
+
+    afterAll(async () => {
+      await clearTable(db, Document.tableName);
+      await clearTable(db, Experience.tableName);
+    });
+
+    test.each([
+      [0, []],
+      [textSnippets.length, textSnippets],
+    ])(
+      'Gets all %i text snippets for an experience for a particular document.',
+      async (_length, textSnippetsToAdd) => {
+        // Arrange
+        let nextPosition = 0;
+
+        for (const textSnippetToAdd of textSnippetsToAdd) {
+          const addedTextSnippet = await TextSnippet.add(textSnippetToAdd);
+          await Experience_X_Text_Snippet.add({
+            documentXExperienceId: addedDocumentXExperience.id,
+            textSnippetId: addedTextSnippet.id,
+            textSnippetVersion: addedTextSnippet.version,
+            position: nextPosition++,
+          });
+        }
+
+        // Act
+        const textSnippets = await TextSnippet.getAllForExperienceInDocument(
+          users[0].username,
+          addedDocument.id,
+          addedExperience.id
+        );
+
+        // Assert
+        const expectedTextSnippets = textSnippetsToAdd.map(
+          (textSnippetToAdd) =>
+            new TextSnippet(
+              expect.any(Number),
+              expect.any(Date),
+              textSnippetToAdd.owner,
+              null,
+              textSnippetToAdd.type,
+              textSnippetToAdd.content
+            )
+        );
+
+        expect(textSnippets).toStrictEqual(expectedTextSnippets);
+      }
+    );
+  });
 
   // -------------------------------------------------- delete
 
@@ -54,7 +181,7 @@ describe(className, () => {
         "versions' parent property to null.",
       async () => {
         // Arrange
-        const oldTextSnippet = await ClassRef.add(dataForNewInstances[0]);
+        const oldTextSnippet = await TextSnippet.add(dataForNewInstances[0]);
         const newTextSnippet = await oldTextSnippet.update(dataForUpdate[0]);
 
         newTextSnippet.parent = null;
